25be36d4dde71d8d2e33258daea46b915a6616cd
licyk <15177915035@163.com>
2023-07-24 18:14:26 +0800

    fix bug

--- term-sd.sh
+++ term-sd.sh
@@ -1656,8 +1656,8 @@ function extension_dep_install()
     do
         [ -f "$extension_folder" ] && continue #排除文件
         cd "$extension_folder"
-        echo "安装"$extension_folder"插件依赖"
         if [ -f requirements.txt ];then
+            echo "安装"$extension_folder"插件依赖"
             pip install -r requirements.txt
         fi
         cd ..
