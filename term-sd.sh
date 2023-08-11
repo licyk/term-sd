c39e10152db4b42fc4d0b550ea56f2238a574098
licyk <15177915035@163.com>
2023-08-11 17:49:43 +0800

    去除sd webui安装部分的无用依赖

--- term-sd.sh
+++ term-sd.sh
@@ -1670,7 +1670,7 @@ function process_install_a1111_sd_webui()
     #安装的依赖参考"stable-diffusion-webui/modules/launch_utils.py"
     mkdir ./stable-diffusion-webui/repositories
     #git clone "$github_proxy"https://github.com/CompVis/stable-diffusion.git ./stable-diffusion-webui/repositories/stable-diffusion
-    git clone "$github_proxy"https://github.com/CompVis/taming-transformers.git ./stable-diffusion-webui/repositories/taming-transformers
+    #git clone "$github_proxy"https://github.com/CompVis/taming-transformers.git ./stable-diffusion-webui/repositories/taming-transformers
     git clone "$github_proxy"https://github.com/sczhou/CodeFormer.git ./stable-diffusion-webui/repositories/CodeFormer
     git clone "$github_proxy"https://github.com/salesforce/BLIP.git ./stable-diffusion-webui/repositories/BLIP
     git clone "$github_proxy"https://github.com/Stability-AI/stablediffusion.git/ ./stable-diffusion-webui/repositories/stable-diffusion-stability-ai
