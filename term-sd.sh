95808f7738e7bbd25d2f255a3ca265de32c7e415
licyk <15177915035@163.com>
2023-08-29 13:40:29 +0800

    commit

--- term-sd.sh
+++ term-sd.sh
@@ -958,10 +958,11 @@ function set_proxy_option()
         #pip config set global.index-url "https://mirror.sjtu.edu.cn/pypi/web/simple"
         pip config set global.index-url "https://mirrors.bfsu.edu.cn/pypi/web/simple"
         #pip config set global.extra-index-url "https://mirror.sjtu.edu.cn/pytorch-wheels"
-        pip config set global.extra-index-url "https://mirrors.aliyun.com/pytorch-wheels"
+        #pip config set global.extra-index-url "https://mirrors.aliyun.com/pytorch-wheels"
+        pip config set global.find-links "https://mirrors.aliyun.com/pytorch-wheels/torch_stable.html"
     else
         pip config unset global.index-url
-        pip config unset global.extra-index-url
+        pip config unset global.find-links
     fi
     mainmenu
 }
