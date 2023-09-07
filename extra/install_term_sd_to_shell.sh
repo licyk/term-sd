3ac798d70bf7e0733d1069ee95356270a1cfd931
licyk <15177915035@163.com>
2023-09-07 20:20:20 +0800

    commit

--- extra/install_term_sd_to_shell.sh
+++ extra/install_term_sd_to_shell.sh
@@ -1,17 +1,5 @@
 #/bin/bash
 
-term_sd_install_path=$(pwd) #读取term-sd安装位置
-
-#将要向.bashrc写入的配置
-term_sd_shell_config="termsd(){ user_input_for_term_sd=$(echo \"\$1 \$2 \$3 \$4 \$5 \$6 \$7 \$8 \$9\") ; term_sd_start_path=\$(pwd) ; cd \"$term_sd_install_path\" ; ./term-sd.sh \$user_input_for_term_sd ; cd \"\$term_sd_start_path\" > /dev/null ; }"
-
-user_shell=$(echo $SHELL | awk -F "/" '{print $NF}') #读取用户所使用的shell
-
-if [ ! -z $user_shell = bash ] || [ ! -z $user_shell = zsh ];then
-    echo "不支持该shell"
-    exit 1
-fi
-
 function mainmenu()
 {
     echo "是否将快捷指令添加到shell环境中?"
@@ -22,7 +10,7 @@ function mainmenu()
     echo "提示:输入数字后回车"
     read -p "==>" install_to_shell_option
 
-    if [ -z $install_to_shell_option ];then
+    if [ ! -z $install_to_shell_option ];then
         if [ $install_to_shell_option = 1 ];then
             install_config_to_shell
             exit 1
@@ -59,7 +47,7 @@ function install_config_to_shell()
             echo "配置添加完成,重启shell以生效"
         fi
     fi
-    cd -
+    cd - > /dev/null
 }
 
 function remove_config_from_shell()
@@ -67,5 +55,21 @@ function remove_config_from_shell()
     cd ~
     sed -i '/termsd(){/d' ."$user_shell"rc
     echo "配置已删除,重启shell以生效"
-    cd -
+    cd - > /dev/null
 }
+
+#################################################
+
+term_sd_install_path=$(pwd) #读取term-sd安装位置
+
+#将要向.bashrc写入的配置
+term_sd_shell_config="termsd(){ user_input_for_term_sd=$(echo \"\$1 \$2 \$3 \$4 \$5 \$6 \$7 \$8 \$9\") ; term_sd_start_path=\$(pwd) ; cd \"$term_sd_install_path\" ; ./term-sd.sh \$user_input_for_term_sd ; cd \"\$term_sd_start_path\" > /dev/null ; }"
+
+user_shell=$(echo $SHELL | awk -F "/" '{print $NF}') #读取用户所使用的shell
+
+if [ $user_shell = bash ] | [ $user_shell = zsh ];then
+    mainmenu
+else
+    echo "不支持该shell"
+    exit 1
+fi
