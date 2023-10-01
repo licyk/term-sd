05c6bc6efb1e14806323321c51ec73e3cf827840
licyk <15177915035@163.com>
2023-10-01 11:16:27 +0800

    commit

--- modules/install_prepare.sh
+++ modules/install_prepare.sh
@@ -29,7 +29,7 @@ function proxy_option()
         #pip_mirror="-i https://mirror.sjtu.edu.cn/pypi/web/simple" #上海交大的镜像源有点问题,在安装invokeai时会报错,可能是软件包版本的问题
         #extra_pip_mirror="-f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html"
         pip_index_mirror="--index-url https://pypi.python.org/simple"
-        pip_extra_index_mirror="--extra-index-url="https://pypi.tuna.tsinghua.edu.cn/simple,\ https://mirrors.pku.edu.cn/pypi/web/simple,\ https://mirrors.bfsu.edu.cn/pypi/web/simple""
+        pip_extra_index_mirror="--extra-index-url https://pypi.tuna.tsinghua.edu.cn/simple --extra-index-url https://mirrors.pku.edu.cn/pypi/web/simple --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple"
         pip_find_mirror="--find-links https://mirrors.aliyun.com/pytorch-wheels/torch_stable.html"
         final_install_check_python="启用"
         ;;
