1cd38a90bc6629987ee722b43f0ba4acb092361f
licyk <15177915035@163.com>
2023-10-02 09:39:29 +0800

    commit

--- modules/a1111_sd_webui_launch.sh
+++ modules/a1111_sd_webui_launch.sh
@@ -151,7 +151,7 @@ function generate_a1111_sd_webui_launch()
 #a1111-sd-webui启动界面
 function a1111_sd_webui_launch()
 {
-    a1111_sd_webui_launch_=$(dialog --clear --title "A1111-SD-Webui管理" --backtitle "A1111-SD-Webui启动选项" --ok-label "确认" --cancel-label "取消" --menu "请选择启动A1111-SD-Webui/修改A1111-SD-Webui启动参数\n当前启动参数:\npython $(cat ./term-sd-launch.conf)" 22 70 12 \
+    a1111_sd_webui_launch_=$(dialog --clear --title "A1111-SD-Webui管理" --backtitle "A1111-SD-Webui启动选项" --ok-label "确认" --cancel-label "取消" --menu "请选择启动A1111-SD-Webui/修改A1111-SD-Webui启动参数\n当前启动参数:\n$python_cmd $(cat ./term-sd-launch.conf)" 22 70 12 \
         "1" "启动" \
         "2" "修改启动参数" \
         "5" "返回" \
--- modules/comfyui_launch.sh
+++ modules/comfyui_launch.sh
@@ -91,7 +91,7 @@ function generate_comfyui_launch()
 #comfyui启动界面
 function comfyui_launch()
 {
-    comfyui_launch_=$(dialog --clear --title "ComfyUI管理" --backtitle "ComfyUI启动选项" --ok-label "确认" --cancel-label "取消" --menu "请选择启动ComfyUI/修改ComfyUI启动参数\n当前启动参数:\npython $(cat ./term-sd-launch.conf)" 22 70 12 \
+    comfyui_launch_=$(dialog --clear --title "ComfyUI管理" --backtitle "ComfyUI启动选项" --ok-label "确认" --cancel-label "取消" --menu "请选择启动ComfyUI/修改ComfyUI启动参数\n当前启动参数:\n$python_cmd $(cat ./term-sd-launch.conf)" 22 70 12 \
         "1" "启动" \
         "2" "修改启动参数" \
         "5" "返回" \
