19110b46c89f61e0a335734394438d8770fcb6af
licyk <15177915035@163.com>
2023-10-11 10:39:07 +0800

    commit

--- modules/install_python_package.sh
+++ modules/install_python_package.sh
@@ -7,16 +7,17 @@ function reinstall_python_packages()
     pip_install_methon #安装方式选择
 
     reinstall_python_packages_names=$(dialog --clear --title "Term-SD" --backtitle ""$term_sd_manager_info" pip软件包重装选项" --ok-label "确认" --cancel-label "取消" --inputbox "请输入需要重装的pip软件包名" 25 70 3>&1 1>&2 2>&3)
-    if [ ! -z "$(echo $reinstall_python_packages_names | awk '{gsub(/[=+<>]/, "")}1')" ];then
+    if [ $? = 0 ] && [ ! -z "$(echo $reinstall_python_packages_names | awk '{gsub(/[=+<>]/, "")}1')" ];then
         print_line_to_shell "pip软件包安装"
+        term_sd_notice "安装$reinstall_python_packages_names中"
         enter_venv
         "$pip_cmd" install $reinstall_python_packages_names $pip_index_mirror $pip_extra_index_mirror $pip_find_mirror $force_pip $pip_install_methon_select --force-reinstall --default-timeout=100 --retries 5
         if [ $? = 0 ];then
             print_line_to_shell
-            dialog --clear --title "Term-SD" --backtitle "pip软件包安装结果" --ok-label "确认" --msgbox ""$term_sd_manager_info"安装成功" 25 70
+            dialog --clear --title "Term-SD" --backtitle "pip软件包安装结果" --ok-label "确认" --msgbox "以下python软件包安装成功\n------------------------------------------------------------------\n$reinstall_python_packages_names\n------------------------------------------------------------------" 25 70
         else
             print_line_to_shell
-            dialog --clear --title "Term-SD" --backtitle "pip软件包安装结果" --ok-label "确认" --msgbox ""$term_sd_manager_info"安装失败" 25 70
+            dialog --clear --title "Term-SD" --backtitle "pip软件包安装结果" --ok-label "确认" --msgbox "以下python软件包安装失败\n------------------------------------------------------------------\n$reinstall_python_packages_names\n------------------------------------------------------------------" 25 70
         fi
     fi
 }
