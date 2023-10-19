79534de4809d8e38a36b63bb5c561556dc1726e8
licyk <15177915035@163.com>
2023-10-19 13:06:10 +0800

    commit

--- modules/help.sh
+++ modules/help.sh
@@ -538,10 +538,10 @@ comfyui-extension：安装ComfyUI的插件\n
 \n
 13、unset-pip-path\n
 删除自定义pip解释器路径配置\n
-14、enable-new-bar:\n
+14、enable-new-bar\n
 启用新的Term-SD初始化进度条\n
 \n
-15、disable-new-bar:\n
+15、disable-new-bar\n
 禁用新的Term-SD初始化进度条\n
 \n
 ## 绘世启动器的使用\n
--- term-sd.sh
+++ term-sd.sh
@@ -765,7 +765,7 @@ function term_sd_env_prepare()
 #################################################
 
 #term-sd版本
-term_sd_version_="0.5.5"
+term_sd_version_="0.5.6"
 
 #判断启动状态(在shell中,新变量的值为空,且不需要定义就可以使用,不像c语言中要求那么严格)
 if [ ! -z $term_sd_env_prepare_info ] && [ $term_sd_env_prepare_info = 0 ];then #检测term-sd是直接启动还是重启
