56d78969608ce60dda65f5806ad87e6303efc9f4
licyk <15177915035@163.com>
2023-10-21 00:31:37 +0800

    commit

--- extra/download-hanamizuki.sh
+++ extra/download-hanamizuki.sh
@@ -2,17 +2,19 @@
 
 function download_hanamizuki_resource_select()
 {
-    term_sd_notice "请选择下载源"
+    term_sd_notice "请选择绘世启动器下载源"
     term_sd_notice "1、github源(速度可能比较慢)"
     term_sd_notice "2、gitee源"
     term_sd_notice "3、退出"
     read -p "===============================> " download_hanamizuki_resource_select_
-    if [ ! -z "$download_resource_select_" ];then
-        if [ $download_resource_select_ = 1 ];then
-            download_hanamizuki_resource="https://github.com/licyk/README-collection/releases/download/archive/hanamizuki.exe"
-        elif [ $download_resource_select_ = 2 ];then
-            download_hanamizuki_resource="https://gitee.com/four-dishes/README-collection/releases/download/v0.0.1/hanamizuki.exe"
-        elif [ $download_resource_select_ = 3 ];then
+    if [ ! -z "$download_hanamizuki_resource_select_" ];then
+        if [ $download_hanamizuki_resource_select_ = 1 ];then
+            term_sd_notice "选择github源"
+            export download_hanamizuki_resource="https://github.com/licyk/README-collection/releases/download/archive/hanamizuki.exe"
+        elif [ $download_hanamizuki_resource_select_ = 2 ];then
+            term_sd_notice "选择gitee源"
+            export download_hanamizuki_resource="https://gitee.com/four-dishes/README-collection/releases/download/v0.0.1/hanamizuki.exe"
+        elif [ $download_hanamizuki_resource_select_ = 3 ];then
             download_hanamizuki_info=1
         else
             term_sd_notice "输入有误,请重试"
@@ -27,8 +29,9 @@ function download_hanamizuki()
     aria2c $download_hanamizuki_resource -d ./term-sd-tmp -o "绘世.exe"
     if [ $? = 0 ];then
         if [ -d "./stable-diffusion-webui" ];then
-            if [ ! -f "./stable-diffusion-webui/绘世.exe"];then
+            if [ ! -f "./stable-diffusion-webui/绘世.exe" ];then
                 cp -f "./term-sd-tmp/绘世.exe" ./stable-diffusion-webui
+                term_sd_notice "已将绘世启动器复制到stable-diffusion-webui文件夹"
             else
                 term_sd_notice "stable-diffusion-webui文件夹已存在绘世启动器"
             fi
@@ -37,8 +40,9 @@ function download_hanamizuki()
         fi
 
         if [ -d "./ComfyUI" ];then
-            if [ ! -f "./ComfyUI/绘世.exe"];then
+            if [ ! -f "./ComfyUI/绘世.exe" ];then
                 cp -f "./term-sd-tmp/绘世.exe" ./ComfyUI
+                term_sd_notice "已将绘世启动器复制到ComfyUI文件夹"
             else
                 term_sd_notice "ComfyUI文件夹已存在绘世启动器"
             fi
@@ -51,16 +55,21 @@ function download_hanamizuki()
     rm -rf ./term-sd-tmp    
 }
 
-if [ -d "./stable-diffusion-webui" ] || [ -d "./ComfyUI" ];then
-    if [ ! -f "./stable-diffusion-webui/绘世.exe"] || [ ! -f "./ComfyUI/绘世.exe" ];then
-        download_hanamizuki_info=0
-        download_hanamizuki_resource_select
-        if [ ! $download_hanamizuki_info = 1 ];then
-            download_hanamizuki
+function download_hanamizuki_init()
+{
+    if [ -d "./stable-diffusion-webui" ] || [ -d "./ComfyUI" ];then
+        if [ ! -f "./stable-diffusion-webui/绘世.exe" ] || [ ! -f "./ComfyUI/绘世.exe" ];then
+            download_hanamizuki_info=0
+            download_hanamizuki_resource_select
+            if [ ! $download_hanamizuki_info = 1 ];then
+                download_hanamizuki
+            fi
+        else
+            term_sd_notice "绘世启动器已存在stable-diffusion-webui文件夹中或者ComfyUI文件夹中"
         fi
     else
-        term_sd_notice "绘世启动器已存在"
+        term_sd_notice "未找到stable-diffusion-webui文件夹或者ComfyUI文件夹"
     fi
-else
-    term_sd_notice "未找到stable-diffusion-webui文件夹或者ComfyUI文件夹"
-fi
+}
+
+download_hanamizuki_init
