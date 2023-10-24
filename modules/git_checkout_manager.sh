a45a12c846e689b29c403cd5152928ba57bb1ce2
licyk <15177915035@163.com>
2023-10-24 11:46:32 +0800

    commit

--- modules/git_checkout_manager.sh
+++ modules/git_checkout_manager.sh
@@ -8,7 +8,7 @@ function git_checkout_manager()
 
     commit_selection=$(
         dialog --clear --title "Term-SD" --backtitle "项目切换版本选项" --ok-label "确认" --cancel-label "取消" --menu "请选择要切换的版本\n当前版本:\n$(git show -s --format="%H %cd" --date=format:"%Y-%m-%d %H:%M:%S")" 25 70 10 \
-        $(git log --date=short --pretty=format:"%H %cd" --date=format:"%Y-%m-%d|%H:%M:%S" | awk -F  ' ' ' {print $1 " " $2} ') \
+        $(git log --all --date=short --pretty=format:"%H %cd" --date=format:"%Y-%m-%d|%H:%M:%S" | awk -F  ' ' ' {print $1 " " $2} ') \
         3>&1 1>&2 2>&3)
 
     if [ $? = 0 ];then
