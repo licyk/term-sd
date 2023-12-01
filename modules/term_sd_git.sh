071514dcc01653248aaccb01a8ecb6ba38d4b1d7
licyk <15177915035@163.com>
2023-12-01 23:46:15 +0800

    commit

--- modules/term_sd_git.sh
+++ modules/term_sd_git.sh
@@ -42,11 +42,14 @@ git_ver_switch()
 # git分支签出恢复
 git_fix_pointer_offset()
 {
+    local repo_main_branch
     # 当git在子文件夹中找不到.git文件夹时,将会自动在父文件夹中寻找,以此类推,直到找到.git文件夹。用户的安装方式可能是直接下载源码压缩包,导致安装后的文件夹没有.git文件夹,直接执行git会导致不良的后果
     if [ -d "./.git" ];then # 检测目录中是否有.git文件夹
         term_sd_echo "修复$(git remote -v | awk 'NR==1 {print $2}' | awk -F "/" '{print $NF}' | awk '{sub(".git","")}1')分支签出状态"
         git submodule init # 初始化git子模块
-        git checkout $(git branch -a | grep /HEAD | awk -F'/' '{print $NF}') # 查询远程HEAD所指分支并切换过去
+        repo_main_branch=$(git branch -a | grep /HEAD | awk -F'/' '{print $NF}') # 查询远程HEAD所指分支
+        git checkout $repo_main_branch # 切换到主分支
+        git reset --recurse-submodules --hard origin/$repo_main_branch # 回退到远程分支的版本
         git reset --recurse-submodules --hard HEAD # 回退版本,解决git pull异常
         git restore --recurse-submodules --source=HEAD :/ # 重置工作区
         term_sd_echo "修复$(git remote -v | awk 'NR==1 {print $2}' | awk -F "/" '{print $NF}' | awk '{sub(".git","")}1')完成"
