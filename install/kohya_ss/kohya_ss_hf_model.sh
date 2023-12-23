cfb1ba98fe4ff9b488f20ca56f8c5b61abe12d6f
licyk <15177915035@163.com>
2023-12-23 11:51:01 +0800

    commit

--- install/kohya_ss/kohya_ss_hf_model.sh
+++ install/kohya_ss/kohya_ss_hf_model.sh
@@ -2,6 +2,6 @@ __term_sd_task_sys term_sd_echo "下载模型中"
 __term_sd_task_sys term_sd_echo "使用huggingface模型下载源"
 __term_sd_task_sys term_sd_tmp_enable_proxy
 __term_sd_task_pre_model aria2_download https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors "$kohya_ss_folder"/models/ v1-5-pruned-emaonly.safetensors
-__term_sd_task_pre_model aria2_download https://huggingface.co/licyk/sd-model/resolve/main/sd_1.5/animefull-final-pruned.ckpt "$kohya_ss_folder"/models/ animefull-final-pruned.ckpt
+__term_sd_task_pre_model aria2_download https://huggingface.co/licyk/sd-model/resolve/main/sd_1.5/animefull-final-pruned.safetensors "$kohya_ss_folder"/models/ animefull-final-pruned.safetensors
 __term_sd_task_pre_model aria2_download https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors "$kohya_ss_folder"/models/ vae-ft-mse-840000-ema-pruned.safetensors
 __term_sd_task_pre_model aria2_download https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors "$kohya_ss_folder"/models/ vae-ft-ema-560000-ema-pruned.safetensors
