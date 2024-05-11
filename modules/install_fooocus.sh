a51003bab3e7b38a73883666fd00fd5a6cabc146
licyk <licyk233@163.com>
2024-05-12 01:38:34 +0800

    update fooocus config

--- modules/install_fooocus.sh
+++ modules/install_fooocus.sh
@@ -806,7 +806,8 @@ fooocus_lang_config_file()
     "47 API endpoints": "47 个 API 接口",
     "copy": "复制",
     "copied!": "已复制！",
-    "JSON.parse: unexpected character at line 1 column 1 of the JSON data": "JAVA 解析：JSON 数据第 1 行第 1 列出现意外字符"
+    "JSON.parse: unexpected character at line 1 column 1 of the JSON data": "JAVA 解析：JSON 数据第 1 行第 1 列出现意外字符",
+    "Generate forever": "无限生成"
 }
 EOF
 }
