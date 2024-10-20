b3437a1514a9343fc85bb7403f9ac13ba344ccf1
licyk <licyk233@163.com>
2024-10-20 16:06:58 +0800

    fix python detect

--- modules/term_sd_python_cmd.sh
+++ modules/term_sd_python_cmd.sh
@@ -7,10 +7,10 @@ term_sd_python() {
     if [[ ! -z "${VIRTUAL_ENV}" ]]; then # 当处在虚拟环境时
         # 检测使用哪种命令调用python
         # 调用虚拟环境的python
-        if [[ ! -z "$(python3 --version 2> /dev/null)" ]]; then
-            python3 "$@" # 加双引号防止运行报错
-        elif [[ ! -z "$(python --version 2> /dev/null)" ]]; then
-            python "$@"
+        if [[ ! -z "$(python --version 2> /dev/null)" ]]; then
+            python "$@" # 加双引号防止运行报错
+        elif [[ ! -z "$(python3 --version 2> /dev/null)" ]]; then
+            python3 "$@"
         fi
     else # 不在虚拟环境时
         #调 用系统中存在的python
