4855ba08c07af9b9720aaeb485941fe9ebf06475
licyk <licyk233@163.com>
2026-04-11 21:41:26 +0800

    update pypi mirror url

--- term-sd.sh
+++ term-sd.sh
@@ -1093,7 +1093,7 @@ main() {
     TERM_SD_PIP_EXTRA_INDEX_URL="https://mirrors.cernet.edu.cn/pypi/web/simple"
     # TERM_SD_PIP_FIND_LINKS="https://mirrors.aliyun.com/pytorch-wheels/torch_stable.html https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html"
     TERM_SD_PIP_FIND_LINKS="https://mirrors.aliyun.com/pytorch-wheels/torch_stable.html"
-    TERM_SD_PYPI_MIRROR="https://licyk.github.io/t/pypi_legecy/index.html"
+    TERM_SD_PYPI_MIRROR="https://licyk.github.io/wheels/pypi_legecy/index.html"
     TERM_SD_PIP_INDEX_URL_ARG="" # 用于设置 PyPI 镜像源的命令参数
     TERM_SD_PIP_EXTRA_INDEX_URL_ARG=""
     TERM_SD_PIP_FIND_LINKS_ARG=""
